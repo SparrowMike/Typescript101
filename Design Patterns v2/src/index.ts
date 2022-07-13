d245cb8903313f88ae6be8ab46d10c658f83ba14
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-07-14 00:05:09 +0800

    feat: 🎸 refactoring, splitting the classes

--- Design Patterns v2/src/index.ts
+++ Design Patterns v2/src/index.ts
@@ -1,34 +1,7 @@
-class Sorter {
-    // collection: number[];
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
 
-    // constructor(collection: number[]) {
-    //     this.collection = collection;
-    // }
-
-    constructor(public collection: number[] | string) {}
-
-    sort(): void {
-        const { length } = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) { // type guard 
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand
-                    }
-                }
-                
-                // typeof for number, string, boolean, symbol. instanceof for every other value
-                if (typeof this.collection === 'string') { 
-
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([10, 3, -5, 0])
+const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
-console.log(sorter.collection)
+console.log(numbersCollection.data)
