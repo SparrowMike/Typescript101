5197cda9faa9ee1da1701d9b651bdf7ddcc74a1b
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-07-15 23:04:56 +0800

    feat: 🎸 linkedList

--- Design Patterns v2/src/index.ts
+++ Design Patterns v2/src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 // const numbersCollection = new NumbersCollection([50, 5, 10, 3, -5, 0])
 // const sorter = new Sorter(numbersCollection)
 // sorter.sort()
 // console.log(numbersCollection.data)
 
-const charactersCollection = new CharactersCollection('ScGsabfgfADx')
-const sorter = new Sorter(charactersCollection)
-sorter.sort()
-console.log(charactersCollection.data)
+// const charactersCollection = new CharactersCollection('ScGsabfgfADx')
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+
+const linkedListCollection = new LinkedList();
+linkedListCollection.add(12);
+linkedListCollection.add(51);
+linkedListCollection.add(-23);
+linkedListCollection.add(62);
+
+const sorter = new Sorter(linkedListCollection);
+sorter.sort();
+linkedListCollection.print()
