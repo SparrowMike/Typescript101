d677e8abf0d809f78c59746981273b2556a64ca9
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-07-17 23:29:45 +0800

    refactor: 💡 refactored bubble sort

--- Design Patterns v2/src/index.ts
+++ Design Patterns v2/src/index.ts
@@ -3,15 +3,13 @@ import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
 import { LinkedList } from './LinkedList'
 
-// const numbersCollection = new NumbersCollection([50, 5, 10, 3, -5, 0])
-// const sorter = new Sorter(numbersCollection)
-// sorter.sort()
-// console.log(numbersCollection.data)
+const numbersCollection = new NumbersCollection([50, 5, 10, 3, -5, 0])
+numbersCollection.sort()
+console.log(numbersCollection.data)
 
-// const charactersCollection = new CharactersCollection('ScGsabfgfADx')
-// const sorter = new Sorter(charactersCollection)
-// sorter.sort()
-// console.log(charactersCollection.data)
+const charactersCollection = new CharactersCollection('ScGsabfgfADx')
+charactersCollection.sort()
+console.log(charactersCollection.data)
 
 const linkedListCollection = new LinkedList();
 linkedListCollection.add(12);
@@ -19,6 +17,5 @@ linkedListCollection.add(51);
 linkedListCollection.add(-23);
 linkedListCollection.add(62);
 
-const sorter = new Sorter(linkedListCollection);
-sorter.sort();
+linkedListCollection.sort();
 linkedListCollection.print()
