7705033a11a6e74f5edf5994cdfec8ad68cb9750
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-07-23 19:11:56 +0800

    feat: 🎸 converting the row values and adding type assertion

--- Reusable Code/src/CsvFileReader.ts
+++ Reusable Code/src/CsvFileReader.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import { dateStringToDate } from './utils';
+import { MatchResult } from "./MatchResult";
 
 export class CsvFileReader {
     data: string[][] = [];
@@ -15,6 +17,17 @@ export class CsvFileReader {
                 (row: string): string[] => {
                     return row.split(',');
                 }
-            );
+            )
+            .map((row: string[]): any => {
+                return [
+                    dateStringToDate(row[0]),
+                    row[1],
+                    row[2],
+                    parseInt(row[3]),
+                    parseInt(row[4]),
+                    row[2] as MatchResult, // type assertion
+                    row[6]
+                ]
+            });
     }
 }
