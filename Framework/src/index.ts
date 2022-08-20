d3b7e92043832f11ee7d36cc4dbe5a97f838ddca
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-08-20 23:49:35 +0800

    feat: 🎸 listener support storing event and dynamic array create

--- Framework/src/index.ts
+++ Framework/src/index.ts
@@ -2,7 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({name: 'Jake', age: 20});
 
-user.set({ name: 'Mike' });
+user.on('change', () => {});
+user.on('change', () => {});
+user.on('gasd', () => {});
 
-console.log(user.get('name'))
-console.log(user.get('age'))
+console.log(user)
